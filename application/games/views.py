705291b030976151430ffbba39f51d4ccea28e0a
nakkekakke <juntusen.niko@gmail.com>
2019-03-28 22:00:43 +0200

    lisätty puuttuvat importit

--- application/games/views.py
+++ application/games/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, request, redirect, url_for
+from flask_login import login_required, current_user
 from application import app, db
 from application.games.models import Game
 from application.games.forms import GameForm
